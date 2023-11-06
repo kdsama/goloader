efed827f27144d26a806156501d9d10387707456
kdsama <kdsama5593@gmail.com>
2023-11-06 15:31:37 +0000

    working version of watcher

---
+++ internal/watcher.go
@@ -0,0 +1,108 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+type Watcher struct {
+	directory  string
+	command    string
+	w          *fsnotify.Watcher
+	cmd        *exec.Cmd
+	lastUpdate time.Time
+	ctx        context.Context
+	cancel     context.CancelFunc
+}
+
+func NewWatcher(directory string, cmd string) *Watcher {
+
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	wr := Watcher{
+		directory: directory,
+		w:         w,
+		command:   cmd,
+	}
+
+	return &wr
+}
+
+func (wr *Watcher) Init() {
+
+	if err := wr.w.Add(wr.directory); err != nil {
+		log.Println("Error adding watch:", err)
+	}
+
+}
+func (wr *Watcher) Start() {
+	wr.startCommand()
+	// Start an event loop to handle events
+	for {
+		select {
+		case event, ok := <-wr.w.Events:
+			if !ok {
+				return
+			}
+			if event.Op == fsnotify.Write {
+
+				// if event.Op
+				f := strings.Split(event.Name, ".")
+
+				if f[len(f)-1] == "go" {
+
+					if time.Since(wr.lastUpdate) > 1*time.Second {
+						wr.startCommand()
+					}
+
+				}
+
+			}
+
+		case err, ok := <-wr.w.Errors:
+			if !ok {
+				return
+			}
+			log.Printf("Error: %s\n", err)
+		}
+	}
+}
+
+func (wg *Watcher) startCommand() {
+	cmdArgs := strings.Split(wg.command, " ")
+	if wg.cmd != nil {
+		wg.cmd.Process.Kill()
+
+	}
+	cmd := exec.Command("go", "build", "-o", "./ff", cmdArgs[0])
+	cmd.Dir = wg.directory
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Start()
+
+	wg.cmd = exec.Command("./ff")
+	wg.cmd.Dir = wg.directory
+	wg.cmd.Stdout = os.Stdout
+	wg.cmd.Stderr = os.Stderr
+
+	wg.lastUpdate = time.Now()
+
+	wg.run()
+
+}
+
+func (wg *Watcher) run() {
+	err := wg.cmd.Start()
+	if err != nil {
+		fmt.Println("Process Killed", err)
+	}
+}
--- main.go
+++ main.go
@@ -6,63 +6,70 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/kdsama/goloader/internal"
 )
 
 var cmd *exec.Cmd
 
 func watchSubdirectories(directory string, watcher *fsnotify.Watcher) {
-	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Println("Error walking directory:", err)
-			return err
-		}
-
-		// Check if it's a subdirectory
-		if info.IsDir() && path != directory {
-			// Add the subdirectory to the watcher
-			if err := watcher.Add(path); err != nil {
-				log.Println("Error adding watch:", err)
-			}
-			// log.Printf("Monitoring directory: %s\n", path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Fatal(err)
+	if err := watcher.Add(directory); err != nil {
+		log.Println("Error adding watch:", err)
 	}
+
+	// err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+	// 	if err != nil {
+	// 		log.Println("Error walking directory:", err)
+	// 		return err
+	// 	}
+
+	// 	// Check if it's a subdirectory
+	// 	if info.IsDir() && path != directory {
+	// 		// Add the subdirectory to the watcher
+	// 		fmt.Println("Path-->", path)
+
+	// 		// log.Printf("Monitoring directory: %s\n", path)
+	// 	}
+	// 	return nil
+	// })
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
 
 func startCommand(path string, c string) {
 	cmdArgs := strings.Split(c, " ")
-	fmt.Println(cmdArgs[0], cmdArgs[1:])
-	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
+
+	cmd = exec.Command(cmdArgs[0], cmdArgs[1:]...)
 	cmd.Dir = path
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	fmt.Println("Start")
+	err := cmd.Start()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
+
 }
 
 func restartCommand(path string, c string) {
-	fmt.Println("Restarting", cmd, cmd)
 
 	if cmd != nil && cmd.Process != nil {
+
 		err := cmd.Process.Kill()
+
 		if err != nil {
 			log.Fatal(err)
 		}
-
+		time.Sleep(1 * time.Second)
 		// log.Println("Command stopped")
 
 		// Start the command again
-		fmt.Println(cmd.Args)
+
 		startCommand(path, c)
 	}
 }
@@ -76,52 +83,50 @@ func main() {
 	directory := flag.String("d", "./", "Directory on which hot loader should run ")
 	commandArgs := flag.String("cmd", "", "Go execution command")
 	flag.Parse()
+	wg := internal.NewWatcher(*directory, *commandArgs)
+	wg.Init()
+	wg.Start()
+	// // Create a new watcher
+	// watcher, err := fsnotify.NewWatcher()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer watcher.Close()
 
-	// Create a new watcher
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
+	// // Watch the initial directory
+	// if err := watcher.Add(*directory); err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	// Watch the initial directory
-	if err := watcher.Add(*directory); err != nil {
-		log.Fatal(err)
-	}
-	// log.Printf("Monitoring directory: %s\n", directory)
+	// // Start the initial command
+	// startCommand(*directory, *commandArgs)
 
-	// Watch subdirectories
-	watchSubdirectories(*directory, watcher)
+	// // Start an event loop to handle events
+	// for {
+	// 	select {
+	// 	case event, ok := <-watcher.Events:
+	// 		if !ok {
+	// 			return
+	// 		}
 
-	// Start the initial command
-	go startCommand(*directory, *commandArgs)
+	// 		// Restart the command when an event is caught
+	// 		// log.Printf("Event: %s\n", event)
+	// 		if event.Op == fsnotify.Write {
 
-	// Start an event loop to handle events
-	for {
-		select {
-		case event, ok := <-watcher.Events:
-			if !ok {
-				return
-			}
+	// 			f := strings.Split(event.Name, ".")
 
-			// Restart the command when an event is caught
-			// log.Printf("Event: %s\n", event)
-			if event.Op == fsnotify.Write {
-				// if event.Op
-				f := strings.Split(event.Name, ".")
+	// 			if f[len(f)-1] == "go" {
 
-				if f[len(f)-1] == "go" {
+	// 				restartCommand(*directory, *commandArgs)
+	// 			}
 
-					restartCommand(*directory, *commandArgs)
-				}
+	// 		}
 
-			}
-
-		case err, ok := <-watcher.Errors:
-			if !ok {
-				return
-			}
-			log.Printf("Error: %s\n", err)
-		}
-	}
+	// 	case err, ok := <-watcher.Errors:
+	// 		if !ok {
+	// 			return
+	// 		}
+	// 		log.Printf("Error: %s\n", err)
+	// 	}
+	// }
 }
